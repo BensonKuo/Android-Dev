b28c451326c1b59ac3409188b992841cbc8d27c2
bensonkuo <bensonkuo17@gmail.com>
2015-10-08 21:16:37 +0800

    USE ENTER TO  SUBMIT

--- android_class_261-master/SimpleUI/app/src/main/java/com/example/simpleui/MainActivity.java
+++ android_class_261-master/SimpleUI/app/src/main/java/com/example/simpleui/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.simpleui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -19,13 +20,29 @@ public class MainActivity extends AppCompatActivity {
         // 做出主要畫面 參照activity_main.xml來做  R.layout是參照位置
 
         inputText = (EditText)findViewById(R.id.inputText);  // cast 成edittext 型別
+
+        inputText.setOnKeyListener(new View.OnKeyListener() {  //onkeylistener 是一個介面
+            @Override   // 實作interface的method
+            public boolean onKey(View v, int i, KeyEvent keyEvent) {
+                //先判斷是按下還是放開
+                if (keyEvent.getAction()==keyEvent.ACTION_DOWN){
+                    if (i == KeyEvent.KEYCODE_ENTER){
+                        submit(null); //沒有用到所以填null
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
     }
 
-    public void submit(View v){
+    public void submit(View v){ //view 參數可用來判斷是哪個button被案到了
+
 
         String text = inputText.getText().toString();
-        Toast.makeText(this, text,Toast.LENGTH_LONG).show();
-        //inputText.setText("aaa");
+        Toast.makeText(this,text, Toast.LENGTH_LONG).show();
+
+        inputText.setText("");
 
     }
 
