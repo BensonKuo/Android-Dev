924eae90c156cab78fcff9edb790747a4201882b
bensonkuo <bensonkuo17@gmail.com>
2015-10-29 20:25:34 +0800

    AsyncTask

--- android_class_261-master/SimpleUI/app/src/main/java/com/example/simpleui/OrderDetailActivity.java
+++ android_class_261-master/SimpleUI/app/src/main/java/com/example/simpleui/OrderDetailActivity.java
@@ -1,9 +1,12 @@
 package com.example.simpleui;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+
+import java.security.PrivateKey;
 
 public class OrderDetailActivity extends AppCompatActivity {
 
@@ -20,25 +23,53 @@ public class OrderDetailActivity extends AppCompatActivity {
         final String address = storeInfo.split(",")[1];
         Log.d("debug", address);
 
-        // async task
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                // address -> url
-                String url = Utils.getGEOUrl(address);
-                // url -> bytes ->string
-                final String result = new String(Utils.urlToBytes(url));
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        // string ->jsonobj ->string
-                        addressTextView.setText(Utils.getLatLngFromJSON(result));
-                    }
-                });
+//        // async task
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                // address -> url
+//                String url = Utils.getGEOUrl(address);
+//                // url -> bytes ->string
+//                final String result = new String(Utils.urlToBytes(url));
+//                runOnUiThread(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        // string ->jsonobj ->string
+//                        addressTextView.setText(Utils.getLatLngFromJSON(result));
+//                    }
+//                });
+//
+//            }
+//        }).start();
+//
+        GeoCodingTask task = new GeoCodingTask();
+        task.execute(address);
 
-            }
-        }).start();
+    }
+
+    //<doInBackground參數形式,,onPostExcute參數形式>
+    private class GeoCodingTask extends AsyncTask<String, Integer, String> {
+        @Override
+        protected String doInBackground(String... params){  // connect internet on other thread
+            String url = Utils.getGEOUrl("Taipei");
+            String json = new String(Utils.urlToBytes(url));
+            String latLng = Utils.getLatLngFromJSON(json);
+            return latLng;
+//            String url = Utils.getGEOUrl("Taipei");
+//            String result = new String(Utils.urlToBytes(url));
+//            Log.d("debug", result);
+//            return null;
+        }
+
+        // 執行之後做一些事情
+        @Override
+        protected void onPostExecute(String result) {
+            //super.onPostExecute(aVoid);
+            addressTextView.setText(result);
+        }
+
 
 
     }
+
 }
