8a8f9e276f5c00c826a4b8536b689c9bab6f1d6b
bensonkuo <bensonkuo17@gmail.com>
2015-11-05 21:48:44 +0800

    access token

--- android_class_261-master/SimpleUI/app/src/main/java/com/example/simpleui/LoginActivity.java
+++ android_class_261-master/SimpleUI/app/src/main/java/com/example/simpleui/LoginActivity.java
@@ -3,13 +3,20 @@ package com.example.simpleui;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -23,13 +30,38 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        if (accessToken != null) {
+            GraphRequest.newMeRequest(accessToken, new GraphRequest.GraphJSONObjectCallback() {
+                @Override
+                public void onCompleted(JSONObject object, GraphResponse response) {
+                    String userName = null;
+                    try {
+                        userName = object.getString("name");
+                        Log.d("username",userName);
+                        goToMainActivity(userName);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).executeAsync();
+        }
+
+
         callbackManager = CallbackManager.Factory.create();
         setUpFacebooklLogin();
 
     }
 
-    private void setUpFacebooklLogin(){
-        LoginButton loginButton = (LoginButton)findViewById(R.id.login_button);
+    private void goToMainActivity(String userName) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("username",userName);
+        startActivity(intent);
+    }
+
+    private void setUpFacebooklLogin() {
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
@@ -46,7 +78,6 @@ public class LoginActivity extends AppCompatActivity {
 
             }
         });
-
     }
 
 
